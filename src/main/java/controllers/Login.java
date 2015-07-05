fbcfb589649fcb2b9fd20c5d10794c4da9eb898e
Djordje Miletic <mile_djole@hotmail.com>
2015-07-05 22:18:52 +0200

    Dodat meni i slika.

--- src/main/java/controllers/Login.java
+++ src/main/java/controllers/Login.java
@@ -7,16 +7,20 @@ package controllers;
 
 import DB.DBFactory;
 import DB.Korisnik;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseId;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 import org.primefaces.model.UploadedFile;
 
@@ -209,8 +213,17 @@ public class Login {
         this.file = file;
     }
 
-    public StreamedContent getSlika() {
-        return slika;
+    public StreamedContent getSlika() throws IOException {
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        if (context.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
+            // So, we're rendering the HTML. Return a stub StreamedContent so that it will generate right URL.
+            return new DefaultStreamedContent();
+        } else {
+            // So, browser is requesting the image. Return a real StreamedContent with the image bytes.
+//            String imageId = context.getExternalContext().getRequestParameterMap().get("imageId");
+            return new DefaultStreamedContent(new ByteArrayInputStream(korisnik.getSlika()));
+        }
     }
 
     public void setSlika(StreamedContent slika) {
